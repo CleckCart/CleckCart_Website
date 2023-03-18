0e9fe3f7b5e06d332ee44007b28bebabe4107f41
Promise Rijal <promrizal@gmail.com>
2023-03-18 12:05:19 +0545

    added-javascript-eyetoggle-with-login-modal

--- src/components/customer/PHP/HomePage.php
+++ src/components/customer/PHP/HomePage.php
@@ -4,11 +4,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Web Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="../CSS/links.css">
     <link rel="stylesheet" href="../CSS/homepage.css">
 </head>
 <body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src = "../JS/passwordVisibility.js"></script>
     
     <!--NavBar-->
     <div class = "topbar">
@@ -59,8 +60,60 @@
                         <li class="nav-item me-3">
                             <a class="nav-link" href="#"><img src="../public/heart.svg" alt="heart"></a>
                         </li>
-                        <li class="nav-item me-3">
-                            <a class="nav-link" href="#"><img src="../public/person.svg" alt="person"></a>
+                        <li class="nav-item me-3 ">
+                            <a class="nav-link" data-bs-toggle="modal" data-bs-target="#exampleModal"  href="#"><img src="../public/person.svg" alt="person"></a>
+                                <!-- Modal -->
+                                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="exampleModalLabel">Login</h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <form method = "POST" action = "#">
+                                                    <div class="mb-3">
+                                                        <label for="exampleInputText1" class="form-label">Username</label>
+                                                        <input type="text" class="form-control" placeholder="Username" aria-label="Username">
+                                                    </div>
+                                                    <div class="mb-3">
+                                                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                                                        <div class="input-group">
+                                                            <input type="password" class="form-control" placeholder = "Password" id="password" name="password">
+                                                            <span id="togglePassword">
+                                                                <i class="fa-solid fa-eye" aria-hidden = "true" id = "eye" onclick = "toggle()"></i>
+                                                            </span>
+                                                        </div>
+                                                    </div>
+                                                    <div class="mb-3">
+                                                        <div class="row">
+                                                            <div class="col">
+                                                                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                                                    <label class="form-check-label" for="exampleCheck1">Remember Me</label>
+                                                            </div>
+                                                            <div class="col text-end">
+                                                                <label class="form-link-label" for="exampleLink1"><a class="text-reset" href="#">Forgot Password?</a></label>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="mb-3">
+                                                        <button type="submit" class="btn btn-primary w-100">Log In</button>
+                                                        <div class="d-flex flex-column text-center">
+                                                            <div class="row">
+                                                                <label for="exampleInputText1" class="form-label">Don't Have an Account?</label>
+                                                            </div>
+                                                            <div class="row">
+                                                                <label class="form-link-label" for="exampleLink1">
+                                                                    <a class="fs-6 text-reset" href="#">Sign Up</a>
+                                                                </label>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                         </li>
                         <li class="nav-item me-3">
                             <a class="nav-link" href="#"><img src="../public/cart.svg" alt="cart"></a>
