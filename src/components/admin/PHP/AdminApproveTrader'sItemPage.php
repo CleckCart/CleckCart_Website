04dce44aa9e74231c35e49f093b19fe71ab8ac32
Prashant Bajracharya <royalepaly645@gmail.com>
2023-04-05 20:26:59 +0545

    admin-tables

---
+++ src/components/admin/PHP/AdminApproveTrader'sItemPage.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Approve Products</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+  <link rel="stylesheet" href="../CSS/style.css" />
+</head>
+
+<body>
+  <div class="container-left mainContainer">
+    <a href="#" class="btn" style="margin-left:-10px;"><i class="fa-solid fa-bars" style="font-size:24px;color:grey;"></i></a><br><br>
+    <div class="row bg-success headingContainer">
+      <div class="col">
+        <h1>Approve Products</h1>
+      </div><br />
+      <div class="col" style="margin-top:10px;text-align:right;">
+      </div>
+    </div>
+
+    <table class="table table-light table-striped" style="text-align:center;">
+      <thead class="table-success">
+        <tr>
+          <th>Select</th>
+          <th>ID</th>
+          <th>Image</th>
+          <th>Product Name</th>
+          <th>Trader Username</th>
+          <th>Description</th>
+          <th>Category</th>
+          <th>Price</th>
+          <th>Date</th>
+          <th colspan=2>Actions</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tr>
+        <td></td>
+        <td>1</td>
+        <td>Ipsum.jpg</td>
+        <td>lorem</td>
+        <td>ipsum8</td>
+        <td>Lorem, Ipsum asd asda sda sd asdasdasdasdasd</td>
+        <td>Lorem</td>
+        <td>&pound;100</td>
+        <td>2023/04/04</td>
+        <td><a href="#"><i class="fa-sharp fa-solid fa-circle-check" style="color:green;"></i></i></a></td>
+        <td><a href="#"><i class="fa-solid fa-circle-xmark" style="color:red;"></i></a></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>1</td>
+        <td>Ipsum.jpg</td>
+        <td>lorem</td>
+        <td>ipsum8</td>
+        <td>Lorem, Ipsum asd asda sda sd asdasdasdasdasd</td>
+        <td>Lorem</td>
+        <td>&pound;100</td>
+        <td>2023/04/04</td>
+        <td><a href="#"><i class="fa-sharp fa-solid fa-circle-check" style="color:green;"></i></i></a></td>
+        <td><a href="#"><i class="fa-solid fa-circle-xmark" style="color:red;"></i></a></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>1</td>
+        <td>Ipsum.jpg</td>
+        <td>lorem</td>
+        <td>ipsum8</td>
+        <td>Lorem, Ipsum asd asda sda sd asdasdasdasdasd</td>
+        <td>Lorem</td>
+        <td>&pound;100</td>
+        <td>2023/04/04</td>
+        <td><a href="#"><i class="fa-sharp fa-solid fa-circle-check" style="color:green;"></i></i></a></td>
+        <td><a href="#"><i class="fa-solid fa-circle-xmark" style="color:red;"></i></a></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>1</td>
+        <td>Ipsum.jpg</td>
+        <td>lorem</td>
+        <td>ipsum8</td>
+        <td>Lorem, Ipsum asd asda sda sd asdasdasdasdasd</td>
+        <td>Lorem</td>
+        <td>&pound;100</td>
+        <td>2023/04/04</td>
+        <td><a href="#"><i class="fa-sharp fa-solid fa-circle-check" style="color:green;"></i></i></a></td>
+        <td><a href="#"><i class="fa-solid fa-circle-xmark" style="color:red;"></i></a></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>1</td>
+        <td>Ipsum.jpg</td>
+        <td>lorem</td>
+        <td>ipsum8</td>
+        <td>Lorem, Ipsum asd asda sda sd asdasdasdasdasd</td>
+        <td>Lorem</td>
+        <td>&pound;100</td>
+        <td>2023/04/04</td>
+        <td><a href="#"><i class="fa-sharp fa-solid fa-circle-check" style="color:green;"></i></i></a></td>
+        <td><a href="#"><i class="fa-solid fa-circle-xmark" style="color:red;"></i></a></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>1</td>
+        <td>Ipsum.jpg</td>
+        <td>lorem</td>
+        <td>ipsum8</td>
+        <td>Lorem, Ipsum asd asda sda sd asdasdasdasdasd</td>
+        <td>Lorem</td>
+        <td>&pound;100</td>
+        <td>2023/04/04</td>
+        <td><a href="#"><i class="fa-sharp fa-solid fa-circle-check" style="color:green;"></i></i></a></td>
+        <td><a href="#"><i class="fa-solid fa-circle-xmark" style="color:red;"></i></a></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>1</td>
+        <td>Ipsum.jpg</td>
+        <td>lorem</td>
+        <td>ipsum8</td>
+        <td>Lorem, Ipsum asd asda sda sd asdasdasdasdasd</td>
+        <td>Lorem</td>
+        <td>&pound;100</td>
+        <td>2023/04/04</td>
+        <td><a href="#"><i class="fa-sharp fa-solid fa-circle-check" style="color:green;"></i></i></a></td>
+        <td><a href="#"><i class="fa-solid fa-circle-xmark" style="color:red;"></i></a></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>1</td>
+        <td>Ipsum.jpg</td>
+        <td>lorem</td>
+        <td>ipsum8</td>
+        <td>Lorem, Ipsum asd asda sda sd asdasdasdasdasd</td>
+        <td>Lorem</td>
+        <td>&pound;100</td>
+        <td>2023/04/04</td>
+        <td><a href="#"><i class="fa-sharp fa-solid fa-circle-check" style="color:green;"></i></i></a></td>
+        <td><a href="#"><i class="fa-solid fa-circle-xmark" style="color:red;"></i></a></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>1</td>
+        <td>Ipsum.jpg</td>
+        <td>lorem</td>
+        <td>ipsum8</td>
+        <td>Lorem, Ipsum asd asda sda sd asdasdasdasdasd</td>
+        <td>Lorem</td>
+        <td>&pound;100</td>
+        <td>2023/04/04</td>
+        <td><a href="#"><i class="fa-sharp fa-solid fa-circle-check" style="color:green;"></i></i></a></td>
+        <td><a href="#"><i class="fa-solid fa-circle-xmark" style="color:red;"></i></a></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>1</td>
+        <td>Ipsum.jpg</td>
+        <td>lorem</td>
+        <td>ipsum8</td>
+        <td>Lorem, Ipsum asd asda sda sd asdasdasdasdasd</td>
+        <td>Lorem</td>
+        <td>&pound;100</td>
+        <td>2023/04/04</td>
+        <td><a href="#"><i class="fa-sharp fa-solid fa-circle-check" style="color:green;"></i></i></a></td>
+        <td><a href="#"><i class="fa-solid fa-circle-xmark" style="color:red;"></i></a></td>
+        <td></td>
+      </tr>
+    </table>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+
+</html>
