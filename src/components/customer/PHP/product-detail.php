793e27a13556886c860a6c3f818f9fac2cd2e4e4
Urja Panta <urjapant@gmail.com>
2023-04-24 12:21:28 +0545

    changes-done

---
+++ src/components/customer/PHP/product-detail.php
@@ -0,0 +1,292 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>CleckCart</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Alkatra:wght@400;500;600;700&display=swap"
+      rel="stylesheet"
+    />
+    <link rel = "icon" href = "./../../../dist/public/logo.png" sizes = "16x16 32x32" type = "image/png">
+    <link rel="stylesheet" href="./../../../dist/CSS/bootstrap.css">
+    <link rel="stylesheet" href="../CSS/product-detail.css" />
+  </head>
+<body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<div class="modal">
+  <div class="modal-content">
+    <h2 class="modal-title">Review</h2>
+    <div class="modal-description">
+      <span>Description:</span>
+      <textarea placeholder="Write your review here"></textarea>
+    </div>
+    <div class="modal-rating">
+      <span>Rating:</span>
+      <div class="stars">
+        
+      </div>
+    </div>
+    <div class="modal-buttons">
+      <button class="modal-cancel">Cancel</button>
+      <button class="modal-submit">Submit</button>
+    </div>
+  </div>
+</div>
+
+<div class = "topbar">
+        <nav class="navbar navbar-expand-lg navbar-light bg-my-custom-color">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="./HomePage.php">
+                    <img src="./../../../dist/public/logo.png" class="img-fluid" width = "70" height="70" alt="logo">
+                </a>
+    
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+    
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 mx-auto">
+                        <li class="nav-item me-5">
+                            <a class="nav-link mr-3" aria-current="page" href="./HomePage.php">HOME</a>
+                        </li>
+    
+                        <li class="nav-item dropdown me-5"><!---->
+                            <a class="nav-link mr-3 dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                SHOP
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="./Categories.php">Category</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+    
+                        <li class="nav-item me-5">
+                            <a class="nav-link" href="#">SALE</a>
+                        </li>
+    
+                        <li class="nav-item me-5">
+                            <a class="nav-link mr-3" href="./About.php">ABOUT</a>
+                        </li>
+    
+                        <li class="nav-item me-5">
+                            <a class="nav-link mr-3" href="./Contact.php">CONTACT</a>
+                        </li>
+                    </ul>
+                    
+                    <ul class="d-flex mb-2 mb-lg-0 list-unstyled">
+                        <li class="nav-item me-3">
+                            <a class="nav-link" href="#"><img src="./../../../dist/public/search.svg" alt="search"></a>
+                        </li>
+                        <li class="nav-item me-3">
+                            <a class="nav-link" href="#"><img src="./../../../dist/public/heart.svg" alt="heart"></a>
+                        </li>
+                        <li class="nav-item dropdown me-3"><!---->
+                            <a class="nav-link" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <img src="./../../../dist/public/person.svg" alt="person">
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#myModal">Log In Customer</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">Manage Profile</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">My Orders</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">Log In Trader</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="./Register.php">Sign In</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">Log Out</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item me-5">
+                            <a class="nav-link" href="#"><img src="./../../../dist/public/cart.svg" alt="cart"></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        
+        <!-- Modal -->
+        <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Login</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form method = "POST" action = "#">
+                            <div class="mb-3">
+                                <label for="exampleInputText1" class="form-label">Username</label>
+                                <input type="text" class="form-control" placeholder="Username" aria-label="Username">
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Password</label>
+                                <div class="input-group">
+                                    <input type="password" class="form-control" placeholder = "Password" id="password" name="password">
+                                    <span class = "input-group-text" id="togglePassword">
+                                        <i class="fa-solid fa-eye" aria-hidden = "true" id = "eye" onclick = "toggle()"></i>
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <div class="row">
+                                    <div class="col">
+                                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                            <label class="form-check-label" for="exampleCheck1">Remember Me</label>
+                                    </div>
+                                    <div class="col text-end">
+                                        <label class="form-link-label" for="exampleLink1"><a class="text-reset" href="#">Forgot Password?</a></label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <button type="submit" class="btn btn-primary w-100">Log In</button>
+                                <div class="d-flex flex-column text-center mt-5">
+                                    <div class="row">
+                                        <label for="exampleInputText1" class="form-label">Don't Have an Account?</label>
+                                    </div>
+                                    <div class="row">
+                                        <label class="form-link-label" for="exampleLink1">
+                                            <a class="fs-6 text-reset" href="./Register.php">Sign Up</a>
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  <section class="product-detail">
+    <div class="upper-section">
+        <div class="product-img-cnt">
+          <img class="product-img" src="src/assets/img/bakery.jpg" alt="">
+        </div>
+        <div class="product-info">
+            <div class="product-title-box">
+              <h2 class="product-title">PRODUCT TITLE</h2>
+              <img src="" alt="Favourite icon">
+            </div>
+            <div class="star-rating-box">
+              <p>Star rating <span>No of rating</span></p>
+            </div>
+            <p class="product-description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum incidunt soluta ducimus repellat neque. Doloribus, beatae nostrum vero eius laborum ut error neque debitis laboriosam reprehenderit cupiditate, magni blanditiis vitae.</p>
+           <span class="product-price">$$$</span>
+           <div class="product-quantity">
+            <span>Quantity:</span>
+            <div class="increment-decrement">
+              <span class="decrement-btn">-</span><hr/>
+              <span class="product-quantity-value">1</span><hr/>
+              <span class="increment-btn">+</span>
+              <p>3 in stock</p>
+            </div>
+          </div>
+          <button class="add-to-cart">ADD TO CART</button>
+          <button class="buy-now">BUY NOW</button>
+    </div>
+    </div>
+    <div class="lower-section">
+      <h2 class="review-heading">Customer Reviews</h2>
+      <div class="review">
+        <div class="review-left">
+          <h3 class="review-name">Customer Name</h3>
+          <span class="review-rating">Star rating</span>
+          <span class="review-date">01/01/2023</span>
+        </div>
+        <hr />
+        <div class="review-right">
+            <h4 class="review-title">Review Title</h4>
+            <p class="review-description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis quia doloribus culpa esse rem labore nesciunt quas, numquam aperiam odio nobis obcaecati minus provident nam ab fugiat illo? Atque, enim!</p>
+        </div>
+      </div>
+    </div>
+  </section>
+   <!--footer-->
+   <footer>
+    <div class = "container-fluid bg-secondary">
+            <div class="row row-cols-2 row-cols-md-4 g-4">
+                <div class="col mt-5 text-center">
+                        <div class="d-flex flex-column bd-highlight mb-3">
+                            <div class="p-2 bd-highlight">
+                                    <h3 class = "mt-5">Cleck Cart</h3>
+                                    <h5 class = "mt-5">Satisfy your cravings, with local farm savings</h5>
+                            </div>
+                            <div class="d-flex flex-row flex-wrap p-2 align-self-center">
+                                <a class="nav-link p-3" href="#"><img src="./../../../dist/public/twitter.svg" alt="twitter"></a>
+                                <a class="nav-link p-3" href="#"><img src="./../../../dist/public/facebook.svg" alt="facebook"></a>
+                                <a class="nav-link p-3" href="#"><img src="./../../../dist/public/instagram.svg" alt="instagram"></a>
+                            </div>
+                        </div>
+                </div>
+                <div class="col mt-5 text-center">
+                        <div class="d-flex flex-column bd-highlight mb-3">
+                            <div class="p-2 bd-highlight">
+                                    <h3 class = "mt-5">Join Us</h3>
+                                    <h5 class = "mt-5">Sell on CleckCart</h5>
+                            </div>
+                        </div>
+                </div>
+                <div class="col mt-5 text-center">
+                        <div class="d-flex flex-column bd-highlight mb-3">
+                            <div class="p-2 bd-highlight">
+                                    <h3 class = "mt-5">Help</h3>
+                                    <h5 class = "mt-5">Pick Up Information</h5>
+                                    <h5 class = "mt-2">Lorem ipsum</h5>
+                                    <h5 class = "mt-2">Lorem ipsum</h5>
+                            </div>
+                        </div>
+                </div>
+                <div class="col mt-5 text-center">
+                        <div class="d-flex flex-column bd-highlight mb-3">
+                            <div class="p-2 bd-highlight">
+                                    <h3 class = "mt-5">Send Us a message</h3>
+                            </div>
+                            <div class="p-2 bd-highlight">
+                                <a class="nav-link text-reset text-decoration-none" href="#"><img src="./../../../dist/public/location.svg" alt="twitter"> lorem ipsum </a>
+                                <a class="nav-link text-reset text-decoration-none" href="#"><img src="./../../../dist/public/call.svg" alt="call"> lorem ipsum </a>
+                                <a class="nav-link text-reset text-decoration-none" href="#"><img src="./../../../dist/public/message.svg" alt="instagram"> lorem ipsum </a>
+                            </div>
+                        </div>
+                </div>
+            </div>
+    </div>
+    </footer>
+<script>
+
+  const modalCancelBtn = document.querySelector(".modal-cancel");
+  const modalSubmitBtn = document.querySelector(".modal-submit");
+  const modal = document.getElementById("#modal");
+  const incrementBtn = document.querySelector(".increment-btn");
+  const decrementBtn = document.querySelector(".decrement-btn");
+  const productQuantityValue = document.querySelector(".product-quantity-value");
+
+  modalCancelBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+    modal.style.visibility = "hidden";
+  });
+
+  modalSubmitBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+  });
+
+  incrementBtn.addEventListener("click", () => {
+    productQuantityValue.innerHTML = parseInt(productQuantityValue.innerHTML) + 1;
+  });
+
+  decrementBtn.addEventListener("click", () => {
+    if (parseInt(productQuantityValue.innerHTML) > 1) {
+      productQuantityValue.innerHTML = parseInt(productQuantityValue.innerHTML) - 1;
+    }
+  });
+
+
+</script>
+</body>
+</html>
