062093f1f4957f2ce426c73c4c40dcc685794bac
Jenny <m.jinnyru@gmail.com>
2023-05-03 01:01:26 +0545

    customer-profile

---
+++ src/components/customer/PHP/ProfileUpdate.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CleckCart</title>
+    <link rel="icon" href="./../../../dist/public/logo.png" sizes="16x16 32x32" type="image/png">
+    <link rel="stylesheet" href="./../../../dist/CSS/bootstrap.css">
+    <link rel="stylesheet" href="../CSS/contactpage.css">
+    <style>
+        .error_msg {
+            color: red;
+            font-size: 13px;
+        }
+    </style>
+</head>
+
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="../../service/passwordVisibility.js"></script>
+    <!--NavBar-->
+    <div class="topbar">
+        <nav class="navbar navbar-expand-lg navbar-light bg-my-custom-color">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="./HomePage.php">
+                    <img src="./../../../dist/public/logo.png" class="img-fluid" width="70" height="70" alt="logo">
+                </a>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 mx-auto">
+                        <li class="nav-item me-5">
+                            <a class="nav-link mr-3" aria-current="page" href="./HomePage.php">HOME</a>
+                        </li>
+
+                        <li class="nav-item dropdown me-5"><!---->
+                            <a class="nav-link mr-3 dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                SHOP
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="./Categories.php">Category</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item me-5">
+                            <a class="nav-link" href="#">SALE</a>
+                        </li>
+
+                        <li class="nav-item me-5">
+                            <a class="nav-link mr-3" href="./About.php">ABOUT</a>
+                        </li>
+
+                        <li class="nav-item me-5">
+                            <a class="nav-link mr-3" href="./Contact.php">CONTACT</a>
+                        </li>
+                    </ul>
+
+                    <ul class="d-flex mb-2 mb-lg-0 list-unstyled">
+                        <li class="nav-item me-3">
+                            <a class="nav-link" href="#"><img src="./../../../dist/public/search.svg" alt="search"></a>
+                        </li>
+                        <li class="nav-item me-3">
+                            <a class="nav-link" href="#"><img src="./../../../dist/public/heart.svg" alt="heart"></a>
+                        </li>
+                        <li class="nav-item dropdown me-3"><!---->
+                            <a class="nav-link" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <img src="./../../../dist/public/person.svg" alt="person">
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="./CustomerLogin.php">Log In Customer</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="./ProfilePage.php">Manage Profile</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="./MyOrders.php">My Orders</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="./CustomerLogin.php">Log In Trader</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="./Register.php">Sign Up Customer</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="#">Log Out</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item me-5">
+                            <a class="nav-link" href="#"><img src="./../../../dist/public/cart.svg" alt="cart"></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+    </div>
+
+    <!-- Page content holder -->
+    <div class="page-content p-5" id="content">
+        <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-10">
+                <div class="text-center">
+                    <h5>Update Customer</h5>
+                </div>
+                <hr>
+                <form method="POST" action="ProfileUpdateSubmit.php">
+                    <?php
+                    if (isset($_GET['error'])) { ?>
+                        <div class='alert alert-danger text-center' role='alert'><?php echo ($_GET['error']); ?></div>
+                    <?php } ?>
+                    <?php
+                        if(isset($_GET['success'])) {?>
+                        <div class='alert alert-success text-center' role='alert'><?php echo($_GET['success']);?></div>
+                    <?php }?>
+                    <div class="mb-3">
+                        <div class="row mb-3">
+                            <div class="col">
+                                <label for="exampleInputText1" class="form-label">First Name</label>
+                                <input type="text" class="form-control" placeholder="First Name" aria-label="First name" name="CustomerEditFirstname">
+                            </div>
+                            <div class="col">
+                                <label for="exampleInputText1" class="form-label">Last Name</label>
+                                <input type="text" class="form-control" placeholder="Last Name" aria-label="Last name" name="CustomerEditLastname">
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col">
+                                <label for="exampleInputText1" class="form-label">Username</label>
+                                <input type="tel" class="form-control" placeholder="Username" aria-label="Username" name="CustomerEditUsername">
+                            </div>
+                            <div class="col">
+                                <label for="exampleInputEmail1" class="form-label">Email</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email Address" name="CustomerEditEmail">
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <div class="col">
+                                <label for="exampleInputText1" class="form-label">Address</label>
+                                <input type="tel" class="form-control" placeholder="Address" aria-label="Address" name="CustomerEditAddress">
+                            </div>
+                            <div class="col">
+                                <label for="exampleInputText1" class="form-label">Phone</label>
+                                <input type="tel" class="form-control" placeholder="Phone Number" aria-label="PhoneNumber" name="CustomerEditPhone">
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <div class="row mb-3">
+                                <div class="col">
+                                    <label for="file" class="form-label">Image</label>
+                                    <input type="file" class="form-control" id="file" aria-label="File" name="CustomerEditImage">
+                                </div>
+                                <div class="col">
+                                    <label for="date" class="form-label">Date of Birth</label>
+                                    <input type="date" class="form-control" id="date" aria-label="date" name="CustomerEditDate">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-8" style="margin-top:4vh">
+                        <input type="submit" class="btn btn-primary w-50 " value="Update" name="CustomerEdit">
+                    </div>
+                </form>
+            </div>
+            <div class="col-sm-1"></div>
+        </div>
+    </div>
+
+
+    <!-- footer -->
+    <footer>
+        <footer class="page-footer font-small pt-5">
+
+            <div class="container-fluid bg-secondary">
+                <div class="row row-cols-2 row-cols-md-4 g-4">
+                    <div class="col mt-5 text-center">
+                        <div class="d-flex flex-column bd-highlight mb-3">
+                            <div class="p-2 bd-highlight">
+                                <h3 class="mt-5">Cleck Cart</h3>
+                                <h5 class="mt-5">Satisfy your cravings, with local farm savings</h5>
+                            </div>
+                            <div class="d-flex flex-row flex-wrap p-2 align-self-center">
+                                <a class="nav-link p-3" href="https://twitter.com/" target="_blank"><img src="./../../../dist/public/twitter.svg" alt="twitter"></a>
+                                <a class="nav-link p-3" href="https://www.facebook.com/" target="_blank"><img src="./../../../dist/public/facebook.svg" alt="facebook"></a>
+                                <a class="nav-link p-3" href="https://www.instagram.com/" target="_blank"><img src="./../../../dist/public/instagram.svg" alt="instagram"></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mt-5 text-center">
+                        <div class="d-flex flex-column bd-highlight mb-3">
+                            <div class="p-2 bd-highlight">
+                                <h3 class="mt-5">Join Us</h3>
+                                <h5 class="mt-5">Sell on CleckCart</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mt-5 text-center">
+                        <div class="d-flex flex-column bd-highlight mb-3">
+                            <div class="p-2 bd-highlight">
+                                <h3 class="mt-5">Help</h3>
+                                <h5 class="mt-5">Pick Up Information</h5>
+                                <h5 class="mt-2">Lorem ipsum</h5>
+                                <h5 class="mt-2">Lorem ipsum</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mt-5 text-center">
+                        <div class="d-flex flex-column bd-highlight mb-3">
+                            <div class="p-2 bd-highlight">
+                                <h3 class="mt-5">Send Us a message</h3>
+                            </div>
+                            <div class="p-2 bd-highlight">
+                                <a class="nav-link text-reset text-decoration-none" href="#"><img src="./../../../dist/public/location.svg" alt="twitter"> lorem ipsum </a>
+                                <a class="nav-link text-reset text-decoration-none" href="#"><img src="./../../../dist/public/call.svg" alt="call"> lorem ipsum </a>
+                                <a class="nav-link text-reset text-decoration-none" href="#"><img src="./../../../dist/public/message.svg" alt="instagram"> lorem ipsum </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </footer>
+
+</body>
+
+</html>
