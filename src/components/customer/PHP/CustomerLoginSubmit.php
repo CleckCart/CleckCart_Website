b7d203ecb066dbe315b4293f08ecc6ec09a21775
Promise Rijal <promrizal@gmail.com>
2023-05-03 20:35:34 +0545

    customer-login-added

--- src/components/customer/PHP/CustomerLoginSubmit.php
+++ src/components/customer/PHP/CustomerLoginSubmit.php
@@ -1,6 +1,7 @@
 <?php
  /*Check if form is submitted*/
  if(isset($_POST['CustomerLoginSubmit'])){
+    include('./connectSession.php');
     /*Check if all fields are filled*/ 
     if (empty($_POST['CustomerLoginUsername']) || empty($_POST['CustomerLoginPassword'])){
         header('Location:./CustomerLogin.php?error=Please make sure all text fields are not empty.');
@@ -8,13 +9,31 @@
     else{
         $CustomerLoginUsername = trim(filter_input(INPUT_POST, 'CustomerLoginUsername', FILTER_SANITIZE_STRING));
         $CustomerLoginPassword = trim(filter_input(INPUT_POST, 'CustomerLoginPassword', FILTER_SANITIZE_STRING));
+        $CustomerRole="Customer";
         /*Check if username is of 5-10 characters*/
         if(strlen($CustomerLoginUsername) >= 5 && strlen($CustomerLoginUsername) <= 10){
                 $passwordPattern = '/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,10}$/';
                 /*Check if password has 6 - 10 characters, 1 Uppercase, 1 Lowercase, 1 Number and 1 Special Character.*/
                 if(preg_match($passwordPattern, $CustomerLoginPassword))
                     {
-                    /*For inserting into database*/
+                        if (!$conn) {
+                            $error_message = "Unable to connect to the database.";
+                        } else {
+                            $encryptedPassword = md5($CustomerLoginPassword);
+                            $query = "SELECT * FROM USER_TABLE WHERE USERNAME = '$CustomerLoginUsername' AND PASSWORD = '$encryptedPassword' AND ROLE ='$CustomerRole'";
+                            $result = oci_parse($conn, $query);
+                            oci_execute($result);
+                            // Fetch the result
+                            $row = oci_fetch_array($result, OCI_ASSOC);
+                            if ($row) {
+                                // If the user is found, create a session
+                                $_SESSION['username'] = $CustomerLoginUsername;
+                            }
+                            else{
+                                    $_SESSION['error'] = 'Invalid Credentials!';
+                            }
+                            header('Location:./Session.php');
+                        }
                     }
                 else
                     {
