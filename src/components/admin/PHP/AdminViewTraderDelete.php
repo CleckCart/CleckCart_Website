2c3d085c3326712efc7eb4380a09cdbb7f184c61
Prashant Bajracharya <royalepaly645@gmail.com>
2023-05-07 14:38:09 +0545

    admin-delet-traders-done

--- src/components/admin/PHP/AdminViewTraderDelete.php
+++ src/components/admin/PHP/AdminViewTraderDelete.php
@@ -7,6 +7,31 @@
   $deleteTraderEmail = $_GET['email'];
   if(isset($_GET['id'])&&isset($_GET['action']))
     {
+  
+      $FetchShopQuery = "SELECT * FROM SHOP WHERE USER_ID = $deleteTraderId";
+      $RunFetchShopQuery = oci_parse($conn, $FetchShopQuery);
+      oci_execute($RunFetchShopQuery);
+      $ShopRow = oci_fetch_array($RunFetchShopQuery, OCI_ASSOC);
+      $Categoryname = $ShopRow['SHOP_NAME'];
+      $ShopId = $ShopRow['SHOP_ID'];
+
+      $DeleteProductQuery = "DELETE FROM PRODUCT WHERE SHOP_ID = $ShopId";
+      $RunDeleteProductQuery = oci_parse($conn, $DeleteProductQuery);
+      oci_execute($RunDeleteProductQuery);
+
+      $DeleteCategoryQuery = "DELETE FROM CATEGORY WHERE CATEGORY_NAME = :CategoryName";
+      $RunDeleteCategoryQuery = oci_parse($conn, $DeleteCategoryQuery);
+      oci_bind_by_name($RunDeleteCategoryQuery,':CategoryName',$Categoryname);
+      oci_execute($RunDeleteCategoryQuery);
+
+      $DeleteShopQuery = "DELETE FROM SHOP WHERE USER_ID = '$deleteTraderId'";
+      $RunDeleteShopQuery = oci_parse($conn, $DeleteShopQuery);
+      oci_execute($RunDeleteShopQuery);  
+     
+      $DeleteUserQuery = "DELETE FROM USER_TABLE WHERE USER_ID = '$deleteTraderId'";     
+      $RunDeleteUserQuery = oci_parse($conn, $DeleteUserQuery);
+      oci_execute($RunDeleteUserQuery);
+
       require '../../../mail/phpmailer/src/Exception.php';
       require '../../../mail/phpmailer/src/PHPMailer.php';
       require '../../../mail/phpmailer/src/SMTP.php';
@@ -23,14 +48,12 @@
       $mail->setFrom('cleckcart@gmail.com'); //sender's email address
       $mail->addAddress($deleteTraderEmail); //reciever's email
       $mail->isHTML(true);
-      $mail->Subject = 'Notice Of Termination! '; //subject of the email for reciever
+      $mail->Subject = 'Notice Of Termination!!! '; //subject of the email for reciever
       $mail->Body = 'Dear, '. $deleteTraderName .'<br>You have been removed from CleckCart as a Trader.<br> Thank you for your time and contribution!'; //message for the reciever
       $mail->send();
-      $sql = "DELETE FROM USER_TABLE WHERE USER_ID = $deleteTraderId";     
-      $DeleteQuery = oci_parse($conn, $sql);
-      oci_execute($DeleteQuery);
-      header("Location:./AdminViewTraderPage.php?success= Trader has been deleted.");
+      //header("Location:./AdminViewTraderPage.php?success= Trader has been deleted.");
     }
+
     else{
       header("Location:./AdminViewTraderPage.php?error= Something went wrong. Please try again.");
     }
