8ab8a2b30e93c525e81af94cb8574288a3906c1a
Promise Rijal <promrizal@gmail.com>
2023-05-09 19:08:38 +0545

    done

--- src/components/customer/PHP/CategoryView.php
+++ src/components/customer/PHP/CategoryView.php
@@ -19,8 +19,9 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <?php
             include('./connect.php');
-            if(isset($_GET['user'])){
+            if(isset($_GET['user']) && isset($_GET['category'])){
                 $user = $_GET['user'];
+                $productCategory = $_GET['category'];
             }
         ?>
         <!--NavBar-->
