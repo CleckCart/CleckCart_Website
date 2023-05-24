a8e0d00f8db4b5787b967f0a449c556b2d23347f
Promise Rijal <promrizal@gmail.com>
2023-05-24 13:14:50 +0545

    done

--- src/components/customer/PHP/PaymentSuccessProcess.php
+++ src/components/customer/PHP/PaymentSuccessProcess.php
@@ -1,22 +1,29 @@
 <?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
     include("./connect.php");
+
     if(isset($_GET['user']) && isset($_GET['cartId']) && isset($_GET['amount']) && isset($_GET['method'])){
         $user = $_GET['user'];
         $cartId = $_GET['cartId'];
-        $productTotalPrice= $_GET['amount'];
+        $productTotalPrice = $_GET['amount'];
         $paymentMethod = $_GET['method'];
 
         $paymentDate = date('Y-m-d');
+
         $queryCustomer = "SELECT * FROM USER_TABLE WHERE USERNAME = '$user' AND ROLE = 'customer'";
         $resultCustomer = oci_parse($conn, $queryCustomer);
         oci_execute($resultCustomer);
+
         while($row = oci_fetch_array($resultCustomer, OCI_ASSOC)){
             $userid = $row['USER_ID'];
+            $Email = $row['EMAIL'];
         }
 
         $queryOrder = "SELECT * FROM ORDER_C WHERE CART_ID = $cartId";
         $resultOrder = oci_parse($conn, $queryOrder);
         oci_execute($resultOrder);
+
         while($rowOrder = oci_fetch_array($resultOrder, OCI_ASSOC)){
             $orderId = $rowOrder['ORDER_ID'];
         }
@@ -42,14 +49,68 @@
         $resultCollection = oci_parse($conn, $queryCollection);
         oci_execute($resultCollection);
         $rowCollection = oci_fetch_assoc($resultCollection);
-        $CollectionId=$rowCollection['COLLECTION_ID'];
-
+        $CollectionId = $rowCollection['COLLECTION_ID'];
+        
         $SlotStatus = 'Y';
         $queryUpdateCollectionSlot = "UPDATE COLLECTION_SLOT SET SLOT_STATUS=:SlotStatus WHERE COLLECTION_ID='$CollectionId'";
         $resultUpdateCollectionSlot = oci_parse($conn, $queryUpdateCollectionSlot);
         oci_bind_by_name($resultUpdateCollectionSlot, ":SlotStatus", $SlotStatus);
         oci_execute($resultUpdateCollectionSlot);
 
+        require '../../../mail/phpmailer/src/Exception.php';
+        require '../../../mail/phpmailer/src/PHPMailer.php';
+        require '../../../mail/phpmailer/src/SMTP.php';
+        
+        $mail = new PHPMailer(true);
+        
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'cleckcart@gmail.com'; //sender's email address
+        $mail->Password = 'jqmuadhegtgyetci'; //app password
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port = '465';
+        
+        $mail->setFrom('cleckcart@gmail.com'); //sender's email address
+        $mail->addAddress($Email); //receiver's email
+        $mail->isHTML(true);
+        $mail->Subject = 'Dear ' . $user .', Your Order ID : ' . $orderId . ' Receipt.'; //subject of the email for the receiver
+
+        $queryOrderProduct = "SELECT * FROM ORDER_PRODUCT WHERE ORDER_ID = $orderId";
+        $resultOrderProduct = oci_parse($conn, $queryOrderProduct);
+        oci_execute($resultOrderProduct);
+
+        $tableContent = '<table>';
+        $tableContent .= '<tr>';
+        $tableContent .= '<th>Product</th>';
+        $tableContent .= '<th>Price</th>';
+        $tableContent .= '<th>Quantity</th>';
+        $tableContent .= '</tr>';
+
+        while($rowOrderProduct = oci_fetch_array($resultOrderProduct, OCI_ASSOC)){
+            $orderProductId = $rowOrderProduct['ORDER_PRODUCT_ID'];
+            $ProductId = $rowOrderProduct['PRODUCT_ID'];
+            $orderProductQuantity = $rowOrderProduct['ORDER_QUANTITY'];
+
+            $queryProduct = "SELECT * FROM PRODUCT WHERE PRODUCT_ID = $ProductId";
+            $resultProduct = oci_parse($conn, $queryProduct);
+            oci_execute($resultProduct);
+
+            while($rowProduct = oci_fetch_array($resultProduct, OCI_ASSOC)){
+                $productName = $rowProduct['PRODUCT_NAME'];
+                $productPrice = $rowProduct['PRODUCT_PRICE'];
+
+                $tableContent .= '<tr>';
+                $tableContent .= '<td>' . $productName . '</td>';
+                $tableContent .= '<td>' . '&pound;'.$productPrice . '</td>';
+                $tableContent .= '<td>' . $orderProductQuantity . '</td>';
+                $tableContent .= '</tr>';
+            }
+        }
+        $tableContent .= '</table>';
+        $mail->Body = $tableContent; //message for the receiver
+        $mail->send();
+        
         header("Location:./PaymentSuccess.php?user=$user&orderId=$orderId&amount=$productTotalPrice");
     }
-    ?>
+?>
